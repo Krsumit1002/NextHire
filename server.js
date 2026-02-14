b9f12067e849755112213840184def3a12bd88ca
sumit kumar <krsumit1002@gmail.com>
2026-02-14 23:38:18 +0530

    Remove node_modules and env from tracking

--- server.js
+++ server.js
@@ -1,13 +1,21 @@
 import express from 'express'
 import dotenv from 'dotenv'
+import connectDB from './config/db.js'
+import testRoutes from './routes/test_routes.js'
+import cors from 'cors'
+import morgan from 'morgan'
 
 dotenv.config()
 
+connectDB()
+
 const app=express()
 
-app.get('/',(req,res)=>{
-    res.send('Welcome to NextHire')
-})
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
+
+app.use('/api/v1',testRoutes)
 
 const PORT=process.env.PORT || 8080
 
